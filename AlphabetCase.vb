5e1f48087867e9c8b1804a3b45645f7de322695f
Tahiralira <52594408+Tahiralira@users.noreply.github.com>
2019-10-01 21:36:44 +0500

    Alphabetcase code
    Alphabet and number code

---
+++ AlphabetCase.vb
@@ -0,0 +1,32 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim Str1, NewChar As String
+        Dim Counter, Alphcount, Numcount As Integer
+
+        Str1 = ""
+        NewChar = ""
+        Counter = 0
+        Alphcount = 0
+        Numcount = 0
+
+        Console.Write("Input String: ")
+        Str1 = Console.ReadLine
+
+        For Counter = 1 To Len(Str1)
+            NewChar = Mid(Str1, Counter, 1)
+            If LCase(NewChar) < "a" Or LCase(NewChar) > "z" Then
+                Numcount = Numcount + 1
+
+            End If
+        Next
+
+        Console.WriteLine(Alphcount)
+        Console.WriteLine(Numcount)
+        Console.ReadKey()
+
+
+
+    End Sub
+
+End Module
