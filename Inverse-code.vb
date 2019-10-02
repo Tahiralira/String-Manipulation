327e561c06ff13a3ad1cd5b9d6dd5cdef7b657cd
Tahiralira <52594408+Tahiralira@users.noreply.github.com>
2019-10-02 21:43:55 +0500

    string inversing code
    This code will,  inverse any letter or number written. for e.g , write zafarAli it will output ilarafaz.

---
+++ Inverse-code.vb
@@ -0,0 +1,32 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim count As Integer
+        Dim Cont1, alph1, Str1, Str2 As String
+
+
+        Str1 = ""
+        Str2 = ""
+        Cont1 = ""
+        alph1 = ""
+        count = 0
+
+
+        Console.Write("Input your String: ")
+        Str1 = Console.ReadLine
+
+        For count = Len(Str1) To 1 Step -1
+            alph1 = Mid(Str1, count, 1)
+            Str2 = Str2 & alph1
+        Next
+
+        Console.WriteLine(Str2)
+        Console.WriteLine(" ")
+
+
+        Console.ReadKey()
+
+    End Sub
+
+End Module
